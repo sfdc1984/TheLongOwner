a813e4328fe0e40f628e70d94f237af323baf0f0
sfdc1984 <sfdc1984@gmail.com>
2026-07-05 19:12:55 +0530

    Update includes.js

--- includes.js
+++ includes.js
@@ -1,1 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
 
+    fetch("components/footer.html")
+        .then(response => response.text())
+        .then(html => {
+            const footer = document.getElementById("footer");
+            if (footer) {
+                footer.innerHTML = html;
+            }
+        });
+
+});
